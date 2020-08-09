445a242dcb75b6f1305e55ac98d4509b8c5e5e38
Sedat <sedaten06@gmail.com>
2020-08-09 22:11:54 +0200

    ikinci satiri ekledim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -3,7 +3,8 @@ package git_project_ikinci;
 public class Deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("ikinci git projem");
+		System.out.println("merhaba");
 
 	}
 
